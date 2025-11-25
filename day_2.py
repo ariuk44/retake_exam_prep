86009132b4b40179236dc9f0a26ef11ca3a1d5ad
ariuk44 <ariunbold.org@gmail.com>
2025-11-25 17:35:50 +0800

    [ADD] day_3

--- day_2.py
+++ day_2.py
@@ -73,3 +73,63 @@ def countSquarePairs(arr):
     return count
 
 print(countSquarePairs([11, 5, 4, 20, 11]))
+
+def countSquarePairs1(arr):
+    cnt = 0
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            x = arr[i]
+            y = arr[j]
+            if x > 0 and y > 0 and isPerfectSquare(x + y):
+                cnt += 1
+    return cnt
+
+print(countSquarePairs1([9, 0, 2, -5, 7]))
+
+def factorial(n):
+    fact = 1
+    for i in range(2, n + 1):
+        fact *= i
+    return fact
+
+def solve10(n):
+    fact10 = factorial(n)
+    for x in range(1, n + 1):
+        for y in range(x, n + 1):
+            if factorial(x) + factorial(y) == fact10:
+                return [x, y]
+    return [0, 0]
+
+print(solve10(10))
+
+def filterArray(arr, n):
+    if n < 0:
+        return None
+    result = []
+    index = 0
+    while n > 0:
+        if n % 2 == 1:
+            if index >= len(arr):
+                return None
+            result.append(arr[index])
+        n //= 2
+        index += 1
+    return result
+
+print(filterArray([8, 4, 9, 0, 3, 1, 2], 88))
+
+def filterArray1(arr, n):
+    if n < 0:
+        return 0
+    result = []
+    index = 0
+    while n > 0:
+        if n % 2 == 1:
+            if index >= len(arr):
+                return None
+            result.append(arr[index])
+        n //= 2
+        index += 1
+    return result
+
+print(filterArray1([0, 9, 12, 18, -6], 11))
